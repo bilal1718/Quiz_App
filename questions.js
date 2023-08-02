3ce203e4f41a872c07685ca4a047a1e2e74bc94e
Muhammad Bilal <muhbil171845@gmail.com>
2023-08-02 09:59:10 +0500

    Adding questions

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        numb:1,
+        question:"Which of the following lie in the Tetra hertz frequency?",
+        answer:"Infra Red and Microwaves",
+        options:[
+           " Visible Light and Infra Red",
+            "Infra Red and Microwaves",
+            "Microwaves and Radio Waves",
+            "Visible Light and Ultraviolet light"
+        ]
+    },
+        {
+            numb:2,
+            question:"What does PHP stands for?",
+            answer:"Hypertext PreProcessor",
+            options:[
+               " HyperText MarkUp Language",
+                "Cascading Style Sheets",
+                "Hypertext PreProcessor",
+                "Hypertext ProPrecessor"
+            ]
+    },
+    {
+        numb:3,
+        question:"What does SQL stands for?",
+        answer:"Structured Query Language",
+        options:[
+           " Stylish Question Language",
+            "Structured Query Language",
+            "Statement Question Language",
+            "Stylesheet Query Language"
+        ]
+},
+{
+    numb:4,
+    question:"Which player has won the Award Of 'Best Player Of Decade' in Cricket?",
+    answer:"Virat Kohli",
+    options:[
+       " Virat Kohli",
+        "Steve Smith",
+        "Baber Azam",
+        "Joe Root"
+    ]
+},
+{
+    numb:5,
+    question:"What is the Shirt Number Of 'Ab De Villiers'?",
+    answer:"17",
+    options:[
+       "45",
+        "18",
+        "17",
+        "56"
+    ]
+}
+];
